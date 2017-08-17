ae7172527cc7d9bc8530efed6dcf28ada0f836c9
danielasocas <daniealsocasgil@gmail.com>
2017-08-17 17:12:12 +0100

    Some imporvements and adding acm file

--- sensemy/timeseries.R
+++ sensemy/timeseries.R
@@ -4,7 +4,7 @@
 #######################################################################
 #                     Preparing data                                  #
 #######################################################################
-V1 <- c(219537531,35078500,35145521,316459169,37141967,41380172,208262837,41380173,37338733,63192525,106334851,37141970,37141970.2)
+V1 <- c(219537531,35078500,35145521,316459169,37141967,41380172,208262837,41380173,37338733,63192525,106334851,37141970,37141970.2, 36979228)
 V2 <- c("Rua da Igreja de Paranhos",
 "Rua Doutor Manuel Pereira Da Silva",
 "Rua D. Roberto Frias 1",
@@ -19,9 +19,21 @@ V2 <- c("Rua da Igreja de Paranhos",
 "Rua D. Eduardo Santos Silva",
 "Rua D. Eduardo Santos Silva 2")
 
-df_osm_edge_ids <- data.frame(matrix(c(V1,V2), nrow = 13))
-df_osm_edge_ids <-data.frame(matrix(c(list_osm_edge,superhot_edges_names), nrow=13))
+V1 <- c(35078500,35145521,316459169,37141967,63192525,106334851,37141970,37141970.2,36979228)
+V2 <- c("Rua Dr. Manuel Pereira Da Silva",
+        "Rua D. Roberto Frias 1",
+        "Rua D. Roberto Frias 2",
+        "Rua Dr. Frei Vicente da Soledade e Castro",
+        "Rua Henrique de Sousa Reis 1",
+        "Rua Henrique de Sousa Reis 2",
+        "Rua D. Eduardo Santos Silva",
+        "Rua D. Eduardo Santos Silva 2",
+        "Rua Dr. Placido da Costa")
+
+
+df_osm_edge_ids <- data.frame(matrix(c(V1,V2), nrow = 9))
 colnames(df_osm_edge_ids) <- c("way_id", "address")
+df_osm_edge_ids <-data.frame(matrix(c(list_osm_edge,superhot_edges_names), nrow=13))
 
 df_osm_edge_ids <- df_osm_edge_ids[c(2:5,10:13),]
 
@@ -273,13 +285,12 @@ df_superhotedges_april16pt_speed %>%
 # New speed per hour. 
 newspeed_avg_h <- function(index) { df_superhotedges_april16pt %>% 
     filter( way_id == df_osm_edge_ids[index,1], wday(time) != 1 , wday(time) != 7 ) %>% 
-    group_by(way_id, session_id, hour = hour(time)) %>% 
+    group_by(way_id, session_id, heading, hour = hour(time)) %>% 
     summarise(median_speed = median(speed*18/5), avg_speed = mean(speed*18/5)) %>% 
-    plot_ly(x= ~factor(hour), y= ~avg_speed, type = 'box', name = "Avg speed per hour (WW)") %>%
+    plot_ly(x= ~factor(hour), y= ~avg_speed,color= ~heading , type = 'box', name = "Avg speed per hour (WW)") %>%
     layout(title = paste("Avg speed per hour",df_osm_edge_ids[index,2],df_osm_edge_ids[index,1], sep = " " ),
            yaxis = list(title="Speed (km/h)", range(0,100)),
-           xaxis = list(title="Hour without weekends", range(0,24))) %>% 
-    add_trace( y= ~mean(avg_speed), name = "Mean of all") 
+           xaxis = list(title="Hour without weekends", range(0,24)))
 }
 
 lapply(1:2, FUN = newspeed_avg_h)
@@ -287,29 +298,27 @@ lapply(1:2, FUN = newspeed_avg_h)
 # New speed per week. 
 newspeed_avg_wd <- function(index) { df_superhotedges_april16pt %>% 
     filter( way_id == df_osm_edge_ids[index,1], wday(time) != 1 , wday(time) != 7 ) %>% 
-    group_by(way_id, session_id, wday = wday(time, label = T)) %>% 
+    group_by(way_id, session_id, heading, wday = wday(time, label = T)) %>% 
     summarise(median_speed = median(speed*18/5), avg_speed = mean(speed*18/5)) %>% 
     plot_ly(x= ~factor(wday), y= ~avg_speed, type = 'box', name = "Avg speed day of the week(WW)") %>%
     layout(title = paste("Avg speed per day of the week:",df_osm_edge_ids[index,2],df_osm_edge_ids[index,1], sep = " " ),
            yaxis = list(title="Speed (km/h)"),
-           xaxis = list(title="Day of the week"))} %>% 
-   add_trace( y= ~mean(avg_speed), name = "Mean of all") 
+           xaxis = list(title="Day of the week"))} 
 
 lapply(1, FUN = newspeed_avg_wd)
 
 # New speed per day. 
 newspeed_avg_d <- function(index) { df_superhotedges_april16pt %>% 
     filter( way_id == df_osm_edge_ids[index,1], wday(time) != 1 , wday(time) != 7 ) %>% 
-    group_by(way_id, session_id, day = day(time)) %>% 
+    group_by(way_id, session_id,heading, day = day(time)) %>% 
     summarise(median_speed = median(speed*18/5), avg_speed = mean(speed*18/5)) %>% 
     plot_ly(x= ~factor(day), y= ~avg_speed, type = 'box', name = "Avg speed day April16 (WW)") %>%
     layout(title = paste("",df_osm_edge_ids[index,2],df_osm_edge_ids[index,1], sep = " " ),
            yaxis = list(title="Speed (km/h)"),
-           xaxis = list(title="Day")) %>% 
-    add_trace( y= ~mean(avg_speed), name = "Mean of all") 
+           xaxis = list(title="Day")) 
 }
 
-lapply(1:2, FUN = newspeed_avg)
+lapply(1:2, FUN = newspeed_avg_d)
 
 #---------------------------------#
 #          Function all           #
@@ -457,9 +466,9 @@ lapply(1, FUN = sd)
 #--- Avg speed / #sessions
 speed_s <-  function(index) { df_superhotedges_april16pt %>% 
   filter( way_id == df_osm_edge_ids[index,1], wday(time) != 1 , wday(time) != 7 ) %>% 
-  group_by(way_id, session_id, hour = hour(time)) %>% 
+  group_by(way_id, session_id, heading, hour = hour(time)) %>% 
   summarise(median_speed = median(speed*18/5), avg_speed = mean(speed*18/5)) %>% 
-  plot_ly(x = ~avg_speed, type = 'histogram', name = "Avg speed April16") %>%
+  plot_ly(x = ~avg_speed,color=~heading,  type = 'histogram', name = "Avg speed April16") %>%
     layout(title = paste("",df_osm_edge_ids[index,2],df_osm_edge_ids[index,1], sep = " " ),
            xaxis = list(title="Average Speed (km/h)"),
            yaxis = list(title="Number of sessions"))
@@ -483,5 +492,105 @@ df_superhotedges_april16pt %>%
 
 ####################################################################
 
+## Low speed and Parkings
 
+df_superhotedges_april16pt %>% 
+  group_by(way_id, session_id, segment_id) %>% 
+  summarise(iqr  = IQR(track), m= mean(speed) ) %>% 
+  filter( iqr == 0 & m== 0) %>% 
+  group_by(way_id) %>% 
+  summarise(n = n()) %>% 
+  arrange(desc(n))
+
+df_superhotedges_april16pt %>% 
+  subset(way_id == 106334851  ) %>% 
+  group_by(session_id) %>% 
+  summarise(median(track),iqr = IQR(track), mean(speed*18/5), median(speed*18/5),  quantile(speed*18/5, 0.75)) %>% 
+  filter(iqr >100)
+  
+  
+df_superhotedges_april16pt %>% 
+  subset(way_id == 106334851 &(session_id == 16665 |session_id == 45313 | session_id == 21481) )
+
+df_superhotedges_april16pt %>% 
+  group_by(way_id, session_id, segment_id) %>% 
+  summarise(iqr  = IQR(track), m= mean(speed) ) %>% 
+  filter( iqr > 100 ) %>% 
+  group_by(way_id) %>% 
+  summarise(n = n()) %>% 
+  arrange(desc(n))
+
+df_superhotedges_april16pt %>% 
+  group_by(way_id, session_id) %>% 
+  summarise(med_tr =median(track),iqr = IQR(track), mean= mean(speed*18/5),median =  median(speed*18/5),q75=  quantile(speed*18/5, 0.75)) %>% 
+  merge(y= df_osm_edge_ids) %>% 
+  ggplot(aes(x=iqr, fill = median)) +
+  geom_histogram(binwidth = 20) + 
+  facet_wrap(~type~address)
+
+# Theme config 
+
+df_intersession_april16pt %>% 
+  filter(intersession_time > 0 ) %>% 
+  merge(df_sessions_edges_april16pt, using = way_id) %>% 
+  ggplot( aes(x=intersession_time, color = category))+
+  scale_x_log10(limits = c(1e0,1e4)) +
+  geom_line(aes(y = 1 - ..y..), stat='ecdf') +
+  scale_color_manual(values = c("grey","blue","red","green"),
+                     breaks = c("SHE", "medium", "low","NA"),
+                     labels = c("High", "Medium", "Low","NA")) +
+  theme(legend.title = element_blank(),
+        legend.text = element_text( size = 20, face = "bold"),
+        legend.position = c(0.8,0.8),
+        axis.title.x = element_text(face="bold", size=20),
+        axis.text.x  = element_text(size=20),
+        axis.title.y = element_text(face="bold", size=20),
+        axis.text.y  = element_text(size=20),
+        panel.background = element_rect(fill = 'white', colour = 'black')) +
+  xlab("Intersession time (min)") + 
+  ylab("P(X>=x)") +
+  geom_vline(xintercept =  c(15,30,45,60), color ="dark grey")
+
+
+#Travelmode
+df_sessions_allmodes_april16pt %>% 
+  filter(travelmode != "NA") %>% 
+  group_by(hour = hour(time_start), travelmode, total_sessions) %>% 
+  summarise(n_sessions = n_distinct(session_id)) %>% 
+  mutate(freq = n_sessions/total_sessions *100) %>% 
+  ggplot(aes(x = hour,y = freq, color=travelmode)) +
+  geom_line() + 
+  ylab("Frequency(%)") +
+  xlab("Hour") +
+  scale_x_continuous(breaks = seq(0,23,1)) + 
+  scale_color_discrete(name = "Travelmode") +
+  theme(axis.title.x = element_text(face="bold", size=20),
+        axis.text.x  = element_text(size=16),
+        axis.title.y = element_text(face="bold", size=20),
+        axis.text.y  = element_text(size=16),
+        legend.title = element_text("Street", size=1, face="bold"), 
+        legend.text = element_text(size=18, face="bold"),
+        panel.background = element_rect(fill = 'white', colour = 'black'),
+        legend.position = c(0.2,0.7))
+
+# Nº sessions 
+df_st_hotedges_april16pt %>% 
+  mutate(class_day = case_when(wday(time_start)!= 1 &  wday(time_start)!= 7 ~ "Weekday",
+                               TRUE ~ "Weekend")) %>% 
+  ggplot(aes(x=hour(time_start))) + 
+  geom_bar(aes(fill = class_trip )) + 
+  scale_fill_hue(breaks = c("1", "2", "3","4"),
+                 labels = c("From campus", "To campus", "Outside campus","Within campus"))+
+  theme(legend.title = element_blank(),
+        legend.text = element_text( size = 20, face = "bold"),
+        legend.position = c(0.75,0.8),
+        axis.title.x = element_text(face="bold", size=20),
+        axis.text.x  = element_text(size=16),
+        axis.title.y = element_text(face="bold", size=20),
+        axis.text.y  = element_text(size=16),
+        panel.background = element_rect(fill = 'white', colour = 'black')) +
+  xlab("Hour") + 
+  ylab("# of sessions") +
+  facet_grid(~as.factor(class_day)) + 
+  theme(strip.text.x = element_text(size = 20))
 
