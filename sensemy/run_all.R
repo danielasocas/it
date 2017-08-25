9407a1cd13b2d2eaccab2b6af1684a8c55094bc0
danielasocas <daniealsocasgil@gmail.com>
2017-08-25 17:31:26 +0100

    Organized and commented

--- sensemy/run_all.R
+++ sensemy/run_all.R
@@ -1,6 +1,6 @@
 source("functions/make_reports.R")
 
-report("rmds/sense_my_feup.Rmd", n_file = "1")
+report("rmds/.Rmd", n_file = "1")
 report("rmds/april2016.Rmd", "2")
 report("rmds/timeseries_light.Rmd", "3")
 report("rmds/preanalysis_topedges.Rmd", "4")
