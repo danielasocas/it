e222954d1d94ae9bab7ca23e30f2190c2d228a82
danielasocas <daniealsocasgil@gmail.com>
2017-10-04 22:48:33 +0100

    source file

---
+++ sensemy/rscripts/main.R
@@ -0,0 +1,20 @@
+"
+Author: Daniela Socas
+Last modified 04/10/2017
+
+"
+
+
+# SOURCING FILES
+
+
+# Loading packages
+source("rscripts/packages.R")
+
+
+# Functions 
+source("functions/.R")
+
+# Pre processing 
+source("rscripts/edges_setup.R")
+source("rscripts/preprocessing.R")
