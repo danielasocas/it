c9c9978bb460c5c39039a8b1a44339c55316452c
danielasocas <daniealsocasgil@gmail.com>
2017-10-11 15:09:29 +0100

    Writing main data as csv (back up).

---
+++ sensemy/rscripts/writing_csv.R
@@ -0,0 +1,7 @@
+# Write to csv 
+
+write.csv(df_speed_top,"df_speed_top.csv")
+write.csv(df_superhotedges_april16pt,"df_superhotedges_april16pt.csv")
+write.csv(df_intersession_april16pt,"df_intersession_april16pt.csv")
+write.csv(df_sessions_edges_april16pt,"df_sessions_edges_april16pt.csv")
+write.csv(dfm_base,"dfm_base.csv")
